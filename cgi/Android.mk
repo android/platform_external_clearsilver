96faf64756cc3bd5eac0fa483566e5e8a106bf5f
Thorsten Glaser <tg@mirbsd.org>
2011-03-15 23:20:37 +0100

    Allow building on i386 Linux targets
    Check $(UNAME) for presence of “64” and assume -m32 otherwise
    
    Part 3 of 7 – make AOSP build on Hardy i386
    
    Change-Id: Ia5e9902ff6d5b024ecfcb10e115d7f15081d3303

--- cgi/Android.mk
+++ cgi/Android.mk
@@ -12,9 +12,14 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/..
 
 LOCAL_CFLAGS := -fPIC
 
+ifneq (64,$(findstring 64,$(UNAME)))
+LOCAL_CFLAGS += -m32
+LOCAL_LDFLAGS += -m32
+else
 # This forces a 64-bit build for Java6
 LOCAL_CFLAGS += -m64
 LOCAL_LDFLAGS += -m64
+endif
 # We use the host compilers because the Linux SDK build
 # uses a 32-bit toolchain that can't handle -m64
 LOCAL_CC := $(CC)
